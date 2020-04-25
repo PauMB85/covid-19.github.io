3773e453c45be5a99c7543ed665aa40c03404c12
PauMB <paumb85@gmail.com>
2020-04-25 21:14:51 +0200

    Updates

---
+++ precache-manifest.2fead18706ecea835d869e4806775df3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b018f66d03bcd3f3522d3dd1ac4b0ea2",
+    "url": "/covid-19.github.io/index.html"
+  },
+  {
+    "revision": "06a2e2824cf52b05b4e8",
+    "url": "/covid-19.github.io/static/css/main.47a87f9f.chunk.css"
+  },
+  {
+    "revision": "750652bcb75697c33c5e",
+    "url": "/covid-19.github.io/static/js/2.8444ca0d.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid-19.github.io/static/js/2.8444ca0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a2e2824cf52b05b4e8",
+    "url": "/covid-19.github.io/static/js/main.3fe1b05c.chunk.js"
+  },
+  {
+    "revision": "6b8d19972cc64fdfe52b",
+    "url": "/covid-19.github.io/static/js/runtime-main.bbd3ac10.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2156b02781f369cf27967d688836a923.js"
+  "/covid-19.github.io/precache-manifest.2fead18706ecea835d869e4806775df3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-19.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
